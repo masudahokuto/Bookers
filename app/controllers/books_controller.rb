f587bf2cb17085cb63c88aef74e02d532a41d83b
EC2 Default User <172101440+masudahokuto@users.noreply.github.com>
2024-07-03 14:57:41 +0000

    [update]3-16

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,27 @@
 class BooksController < ApplicationController
   def new
   end
-  
+
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
-  
+
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
